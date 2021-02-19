492f9c90c16f3cd4a885b82b792b48bd4a32fc70
ytyaru <ytyaru@outlook.jp>
2021-02-19 12:03:01 +0900

    grid

--- docs/7/js/main/index.js
+++ docs/7/js/main/index.js
@@ -5,8 +5,10 @@ import BreakLine from '../novelparser/parser/parts/BreakLine.js';
 import Paragraph from '../novelparser/parser/parts/Paragraph.js';
 window.addEventListener('load', async(event) => {
     const indexes = parseIndexTsv(await FileLoader.load(`./txt/index.tsv`));
+    console.log(indexes );
     createIndex(indexes);
     createIndexAggregate(indexes);
+    createGrid(indexes);
     function createIndex(indexes) {
         document.querySelector('#index').appendChild(createUl(indexes.indexes));
     }
@@ -14,8 +16,8 @@ window.addEventListener('load', async(event) => {
         document.querySelector('#index-aggregate').textContent = `${indexes.indexes.length} 作品`;
     }
     function parseIndexTsv(source) {
+        const allFields = [];
         const indexes = [];
-//        const lines = source.split('\n');
         const lines = source.split('\n').filter(line=>0 < line.trim().length);
         const keys = lines.shift().split('\t');
         const titles = lines.shift().split('\t');
@@ -23,11 +25,10 @@ window.addEventListener('load', async(event) => {
 //        if (lines[0].startsWith('ID	タイトル')) { lines = lines.slice(1); }
         for (const line of lines) {
             const fields = line.split('\t');
+            allFields.push(fields);
             indexes.push(tsvToJson(fields));
         }
-        console.log(indexes);
-//        return indexes;
-        return {indexes: indexes, keys: keys, titles: titles};
+        return {fields: allFields, indexes: indexes, keys: keys, titles: titles};
     }
     function tsvToJson(fields) {
         return {
@@ -41,25 +42,55 @@ window.addEventListener('load', async(event) => {
     }
     function createGrid(indexes) {
         return new gridjs.Grid({
-          columns: [
-            "Name", "Email", "Age", 
-            {
-                name:"Birth",
-    //            formatter: (cell) => `${cell.toISOString()}`
-                formatter: (cell) => `${cell.toLocaleString()}`
-            }
-          ],
+          columns: createGridColumns(indexes),
           sort: true,
-          data: [
-            ["John", "john@example.com", 30, new Date(Date.parse(`2000-01-01 00:00:00`))], 
-            ["Mark", "mark@gmail.com", 20, new Date(Date.parse(`2000-01-01 00:00:01`))],
-            ["Eoin", "eoin@gmail.com", 120, new Date(Date.parse(`2000-01-01 00:00:02`))],
-            ["Sarah", "sarahcdd@gmail.com", 55, new Date(Date.parse(`2000-01-01 00:00:03`))],
-            ["Afshin", "afshin@mail.com", 99, new Date(Date.parse(`2000-01-01 00:00:04`))]
-          ]
+          data: createGridData(indexes),
         }).render(document.getElementById("grid"));
     }
-
+    function createGridColumns(indexes) {
+        const columns = [];
+        for (const [index, key] of indexes.keys.entries()) {
+            if ('created' === key || 'published' === key || 'updated' === key) {
+                columns.push({
+                    name: `${indexes.titles[index]}`, 
+                    formatter: (cell) => `${cell.toLocaleString()}`
+                });
+            } else {
+                columns.push(`${indexes.titles[index]}`);
+            }
+        }
+        return columns;
+    }
+    function createGridData(indexes) {
+        const data = [];
+        for (const [r, fields] of indexes.fields.entries()) {
+            const line = [];
+            for (const [c, field] of fields.entries()) {
+                line.push(convertType(indexes, r, c));
+            }
+            data.push(line);
+//            console.log(i, fields)
+//            data.push(convertType(indexes, i));
+        }
+        console.log(data);
+        return data;
+    }
+    function convertType(indexes, r, c) {
+        if ('id' === indexes.keys[c] || 'count' === indexes.keys[c]) { return parseInt(indexes.fields[r][c]); }
+        else if ('created' === indexes.keys[c]
+              || 'published' === indexes.keys[c]
+              || 'updated' === indexes.keys[c]) { return new Date(Date.parse(indexes.fields[r][c])); }
+        else { return indexes.fields[r][c]; }
+    }
+    /*
+    function convertType(indexes, i) {
+        if ('id' === indexes.keys[i] || 'count' === indexes.keys[i]) { return parseInt(indexes.fields[i]); }
+        else if ('created' === indexes.keys[i]
+              || 'published' === indexes.keys[i]
+              || 'updated' === indexes.keys[i]) { return new Date(Date.parse(indexes.fields[i])); }
+        else { return indexes.fields[i]; }
+    }
+    */
     function createUl(indexes) {
         const ul = document.createElement('ul');
         for (const index of indexes) {
