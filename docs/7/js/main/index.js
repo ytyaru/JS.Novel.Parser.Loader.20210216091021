71c47e3eb9415a1c5ab9d18c46bec015b109960f
ytyaru <ytyaru@outlook.jp>
2021-02-19 11:20:23 +0900

    tsv read

--- docs/7/js/main/index.js
+++ docs/7/js/main/index.js
@@ -8,22 +8,26 @@ window.addEventListener('load', async(event) => {
     createIndex(indexes);
     createIndexAggregate(indexes);
     function createIndex(indexes) {
-        document.querySelector('#index').appendChild(createUl(indexes));
+        document.querySelector('#index').appendChild(createUl(indexes.indexes));
     }
     function createIndexAggregate(indexes) {
-        document.querySelector('#index-aggregate').textContent = `${indexes.length} 作品`;
+        document.querySelector('#index-aggregate').textContent = `${indexes.indexes.length} 作品`;
     }
     function parseIndexTsv(source) {
         const indexes = [];
-        let lines = source.split('\n');
-        if (-1 < lines[0].indexOf('id	title')) { lines = lines.slice(1); }
-        lines = lines.filter(txt=>0 < txt.trim().length);
+//        const lines = source.split('\n');
+        const lines = source.split('\n').filter(line=>0 < line.trim().length);
+        const keys = lines.shift().split('\t');
+        const titles = lines.shift().split('\t');
+//        if (lines[0].startsWith('id	title')) { lines = lines.slice(1); }
+//        if (lines[0].startsWith('ID	タイトル')) { lines = lines.slice(1); }
         for (const line of lines) {
             const fields = line.split('\t');
             indexes.push(tsvToJson(fields));
         }
         console.log(indexes);
-        return indexes;
+//        return indexes;
+        return {indexes: indexes, keys: keys, titles: titles};
     }
     function tsvToJson(fields) {
         return {
@@ -35,6 +39,27 @@ window.addEventListener('load', async(event) => {
             count: fields[5],
         };
     }
+    function createGrid(indexes) {
+        return new gridjs.Grid({
+          columns: [
+            "Name", "Email", "Age", 
+            {
+                name:"Birth",
+    //            formatter: (cell) => `${cell.toISOString()}`
+                formatter: (cell) => `${cell.toLocaleString()}`
+            }
+          ],
+          sort: true,
+          data: [
+            ["John", "john@example.com", 30, new Date(Date.parse(`2000-01-01 00:00:00`))], 
+            ["Mark", "mark@gmail.com", 20, new Date(Date.parse(`2000-01-01 00:00:01`))],
+            ["Eoin", "eoin@gmail.com", 120, new Date(Date.parse(`2000-01-01 00:00:02`))],
+            ["Sarah", "sarahcdd@gmail.com", 55, new Date(Date.parse(`2000-01-01 00:00:03`))],
+            ["Afshin", "afshin@mail.com", 99, new Date(Date.parse(`2000-01-01 00:00:04`))]
+          ]
+        }).render(document.getElementById("grid"));
+    }
+
     function createUl(indexes) {
         const ul = document.createElement('ul');
         for (const index of indexes) {
